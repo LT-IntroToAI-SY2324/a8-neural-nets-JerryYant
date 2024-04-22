190f643a48bbe31503f196d1f9e7495e76f7363e
Jerry Yant <jyantaratan@cps.edu>
2024-04-22 11:15:37 -0500

    troubleshooting

--- a8.py
+++ a8.py
@@ -3,13 +3,15 @@ from neural import NeuralNet
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
 xor_data = [
-    {(0, 0), (0)},
-    {(1, 0), (1)},
-    {(0, 1), (1)},
-    {(0, 0), (0)}
+    [(0, 0), (0)],
+    [(1, 0), (1)],
+    [(0, 1), (1)],
+    [(0, 0), (0)]
 ]
 
 xor_nn = NeuralNet(2, 2, 1)
-xor_nn.train(xor_data, iters=10000, print_interval=1000)
+xor_nn.train(xor_data, iters=1000, print_interval=100)
 
 print(xor_nn.test_with_expected(xor_data))
+
+print("<<<<<<<<<<<<<< Voter Data >>>>>>>>>>>>>>\n")
